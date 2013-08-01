eb600bbb8c4c9c35d7b7fa9ab4ebe37d7c8f81de
Rubify <peter@rubify.com>
2013-08-01 08:49:59 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monittest::Application.config.secret_token = '5d1cce65697c7ea97e816186b41c628935641ea04efccf9ba3a6ba53eb5674adc014a6f58b9b6032a6ad30898531d19eec98943923d4aea1dec9e6c97a48174a'
